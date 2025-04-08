f04a0431d313fad354c373c5acf698ace2deb616
Nicolás Arrieta <nicolas_aarrieta@soy.sena.edu.co>
2025-04-07 21:46:27 -0500

    Función de crear usuario

--- Usuario.php
+++ Usuario.php
@@ -4,13 +4,50 @@ require_once 'database.php';
 
 class Usuario
 {
-    private PDO $conn;
+    public PDO $conn;
+    public $primer_nombre;
+    public $segundo_nombre;
+    public $primer_apellido;
+    public $segundo_apellido;
+    public $n_documento;
+    public $fecha_nacimiento;
+    public $telefono;
+    public $correo_electronico;
+    public $direccion;
 
     public function __construct()
     {
         $this->conn = (new Database())->getConnection();
     }
-
+    
+    public function crearUsuario(){   
+        try{
+           $stm = $this->conn->prepare("INSERT INTO usuarios (
+           primer_nombre, 
+           segundo_nombre, 
+           primer_apellido, 
+           segundo_apellido, 
+           n_documento, 
+           fecha_nacimiento, 
+           telefono, 
+           correo_electronico, 
+           direccion)
+           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stm->execute([
+                $this->primer_nombre, 
+                $this->segundo_nombre, 
+                $this->primer_apellido, 
+                $this->segundo_apellido, 
+                $this->n_documento, 
+                $this->fecha_nacimiento, 
+                $this->telefono, 
+                $this->correo_electronico, 
+                $this->direccion]);
+        
+        } catch (Exception $e) {
+            echo "Error al crear: " . $e->getMessage();
+        }
+    }
     public function listarUsuarios()
     {
         // Lógica para obtener todos los usuarios
@@ -21,10 +58,6 @@ class Usuario
         // Lógica para obtener un usuario por ID
     }
 
-    public function crearUsuario()
-    {
-        // Lógica para insertar usuario
-    }
 
     public function actualizarUsuario()
     {
--- index.php
+++ index.php
@@ -4,6 +4,3 @@ require_once 'Usuario.php';
 
 $usuario = new Usuario();
 
-// Aquí puedes probar:
-// $usuario->crearUsuario("Ana", "ana@mail.com", 22);
-// print_r($usuario->listarUsuarios());
